1da1f789b678d91a57b496e322c22a28d59a24f4
MirzaAyon <ayonmirza98@gmail.com>
2022-08-24 10:58:23 +0600

    not found working

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import { Route, Routes } from "react-router-dom";
 import Home from "./Components/Home/Home"
 import Videos from './Components/Videos/Videos'
 import Login from './Components/Login/Login'
+import NotFound from "./Components/NotFound/NotFound";
 
 export const BlogContext = createContext();
 
@@ -14,6 +15,7 @@ function App() {
       <Route path="/" element={<Home></Home>}></Route>
       <Route path="/videos" element={<Videos></Videos>}></Route>
       <Route path="/login" element={<Login></Login>}></Route>
+      <Route path="*" element={<NotFound></NotFound>}></Route>
     </Routes>
   );
 }
