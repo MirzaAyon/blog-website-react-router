e10a54d2b023927f6bcbe647936db97158ed333d
MirzaAyon <ayonmirza98@gmail.com>
2022-08-24 11:12:38 +0600

    dynamic link worked with react router dom

--- src/Components/Navbar/Navbar.js
+++ src/Components/Navbar/Navbar.js
@@ -1,4 +1,5 @@
 import React from "react";
+import { Link } from "react-router-dom";
 import Logo from "../../Assets/Image/logo.png";
 import "./Navbar.css";
 
@@ -8,7 +9,11 @@ const Navbar = () => {
       <div className='logo-container'>
         <img src={Logo} alt='' />
       </div>
-      <div className='link-container'></div>
+      <div className='link-container'>
+        <Link className="link" to='/'>Home</Link>
+        <Link className="link" to='/login'>Login</Link>
+        <Link className="link" to='/videos'>Videos</Link>
+      </div>
     </nav>
   );
 };
