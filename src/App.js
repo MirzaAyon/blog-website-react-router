b41ad797bbae456f9b9a54017b5c91a590549a68
MirzaAyon <ayonmirza98@gmail.com>
2022-08-24 11:01:21 +0600

    app.js e ekta fragment create korlam instead of div

--- src/App.js
+++ src/App.js
@@ -11,12 +11,15 @@ export const BlogContext = createContext();
 
 function App() {
   return (
-    <Routes>
-      <Route path="/" element={<Home></Home>}></Route>
-      <Route path="/videos" element={<Videos></Videos>}></Route>
-      <Route path="/login" element={<Login></Login>}></Route>
-      <Route path="*" element={<NotFound></NotFound>}></Route>
-    </Routes>
+    <>
+      <Navbar></Navbar>
+      <Routes>
+        <Route path="/" element={<Home></Home>}></Route>
+        <Route path="/videos" element={<Videos></Videos>}></Route>
+        <Route path="/login" element={<Login></Login>}></Route>
+        <Route path="*" element={<NotFound></NotFound>}></Route>
+      </Routes>
+    </>
   );
 }
 
