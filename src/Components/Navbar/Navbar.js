48d319b14b9db028646d4fc6b8f0456b71c3db6f
MirzaAyon <ayonmirza98@gmail.com>
2022-08-24 15:29:11 +0600

    navbar display none applied in read more

--- src/Components/Navbar/Navbar.js
+++ src/Components/Navbar/Navbar.js
@@ -1,11 +1,19 @@
 import React from "react";
-import { NavLink } from "react-router-dom";
+import { NavLink, useLocation } from "react-router-dom";
 import Logo from "../../Assets/Image/logo.png";
 import "./Navbar.css";
 
 const Navbar = () => {
+  // const location = useLocation();
+  // console.log(location);
+  //kothae achi shetar location dibe
+  const { pathname } = useLocation();
+  //use location amdr ke ekta obj return kore 
+  console.log(pathname);
+  //console e jei path e achi ta show korbe jemon /,/videos,/login etc
   return (
-    <nav>
+    <nav style={pathname.includes('blog') ? { display: 'none' } : { display: 'flex' }}>
+      {/* includes er kaj holo oi jinish ta ekhane ache kina ta check kora  */}
       <div className='logo-container'>
         <img src={Logo} alt='' />
       </div>
