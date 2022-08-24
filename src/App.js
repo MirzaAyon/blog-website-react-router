be6434393f472c10caf6e16ed52c06fbbea18ab8
MirzaAyon <ayonmirza98@gmail.com>
2022-08-24 10:45:15 +0600

    videos and login route visible

--- src/App.js
+++ src/App.js
@@ -3,6 +3,8 @@ import Navbar from "./Components/Navbar/Navbar";
 import { createContext } from "react";
 import { Route, Routes } from "react-router-dom";
 import Home from "./Components/Home/Home"
+import Videos from './Components/Videos/Videos'
+import Login from './Components/Login/Login'
 
 export const BlogContext = createContext();
 
@@ -10,6 +12,8 @@ function App() {
   return (
     <Routes>
       <Route path="/" element={<Home></Home>}></Route>
+      <Route path="/videos" element={<Videos></Videos>}></Route>
+      <Route path="/login" element={<Login></Login>}></Route>
     </Routes>
   );
 }
