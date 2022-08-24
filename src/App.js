4d6e43fdcbabff4d75cf6deb6767b02635d456d9
MirzaAyon <ayonmirza98@gmail.com>
2022-08-24 09:54:53 +0600

    Home route shown on UI

--- src/App.js
+++ src/App.js
@@ -1,11 +1,17 @@
 import "./App.css";
 import Navbar from "./Components/Navbar/Navbar";
 import { createContext } from "react";
+import { Route, Routes } from "react-router-dom";
+import Home from "./Components/Home/Home"
 
 export const BlogContext = createContext();
 
 function App() {
-  return <Navbar />;
+  return (
+    <Routes>
+      <Route path="/" element={<Home></Home>}></Route>
+    </Routes>
+  );
 }
 
 export default App;
